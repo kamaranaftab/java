9a214ea0239ce6f97a33e2828e6189d7b2a3d88c
kamaranaftab <150182335+kamaranaftab@users.noreply.github.com>
2023-11-07 11:20:16 -0800

    Add files via upload

---
+++ BasicCalculator.java
@@ -0,0 +1,8 @@
+public class BasicCalculator {
+public static void main(String[] args) {
+int firstNumber = 2;
+int secondNumber = 5;
+int result = firstNumber + secondNumber;
+System.out.println(firstNumber + " + " + secondNumber + " = " + result);
+}
+}
